7a862d7eed103877e6193491f8cbcdfdeb9917c3
dmitriy <dmitriy@markupus.tech>
2020-07-07 17:47:36 +0300

    finish

---
+++ lesson-16/4.js
@@ -0,0 +1,34 @@
+/**
+ * Задача 4.
+ *
+ * Реализуйте класс Stringer, который будет иметь следующие методы (каждый принимает строку в качестве аргумента):
+ * 
+ * - reverse(string) — возвращает строку в перевернутом виде;
+ * - uppercaseFirst(string) — возвращает строку, сделав ее первую букву заглавной;
+ * - uppercaseAll(string) — делает заглавной первую букву каждого слова строки и возвращает её.
+ *
+ * Условия:
+ * - Реализация решения — это синтаксис современных классов JavaScript.
+ */
+
+// Решение
+
+class Stringer {
+  reverse(string) {
+      return string.split('').reverse().join('');
+  }
+
+  uppercaseFirst(string) {
+      return string[0].toUpperCase() + string.slice(1);
+  }
+
+  uppercaseAll(string) {
+      return string.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ')
+  }
+}
+
+const stringer = new Stringer();
+
+console.log(stringer.reverse('good morning!')); // !gninrom doog
+console.log(stringer.uppercaseFirst('good morning!')); // Good morning!
+console.log(stringer.uppercaseAll('good morning!')); // Good Morning!
